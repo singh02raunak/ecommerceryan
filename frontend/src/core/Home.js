0ce7ae25aed6788d3623499b8b146c319394d4e7
raunak <singh02raunak@example.com>
2020-10-09 19:13:22 +0530

    fourth

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from './Layout'
 
 export default function Home() {
+    console.log(123);
     return (
         <div>
             <Layout title="home" description="description ">
